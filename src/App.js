1291b0f5e95ad7de1e5628d992a135832b953240
Ansfred D'Souza <ansfreddsouza@gmail.com>
2022-12-06 17:23:21 +0530

    Pushed All Files Successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+// API Access Key (data.fixer.io) = 0ce522574c9b9c51f85813161ed6c284
 
-function App() {
+import React from 'react';
+import './App.css';
+import CurrencyInput from './CurrencyInput';
+
+// Install 'axios': Command - yarn add axios
+import axios from 'axios';
+
+
+export default function App() {
+  const [amountInInputBox1, setAmountInInputBox1] = React.useState(1);
+  const [amountInInputBox2, setAmountInInputBox2] = React.useState(1);
+  const [currencyInDropDown1, setCurrencyInDropDown1] = React.useState('USD');
+  const [currencyInDropDown2, setCurrencyInDropDown2] = React.useState('EUR');
+  const [rates, setRates] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://data.fixer.io/api/latest?access_key=0ce522574c9b9c51f85813161ed6c284')
+      .then(response => {
+        setRates(response.data.rates);
+      })
+  }, [])
+
+  // For the initial conversion; each time the page is loaded
+  React.useEffect(() => {
+    if (!!rates) {
+      handleAmountInInputBox1Change(1);
+    }
+  }, [rates])
+
+
+  function trimInputBox(number) {
+    return number.toFixed(5);
+  }
+
+  function handleAmountInInputBox1Change(amountInInputBox1) {
+    setAmountInInputBox1(amountInInputBox1);
+    setAmountInInputBox2(trimInputBox(amountInInputBox1 * (rates[currencyInDropDown2] / rates[currencyInDropDown1])));
+  }
+
+  function handleCurrencyInDropDown1Change(currencyInDropDown1) {
+    setCurrencyInDropDown1(currencyInDropDown1);
+    setAmountInInputBox2(trimInputBox(amountInInputBox1 * (rates[currencyInDropDown2] / rates[currencyInDropDown1])));
+  }
+
+  function handleAmountInInputBox2Change(amountInInputBox2) {
+    setAmountInInputBox2(amountInInputBox2);
+    setAmountInInputBox1(trimInputBox(amountInInputBox2 * (rates[currencyInDropDown1] / rates[currencyInDropDown2])));
+  }
+
+  function handleCurrencyInDropDown2Change(currencyInDropDown2) {
+    setCurrencyInDropDown2(currencyInDropDown2);
+    setAmountInInputBox1(trimInputBox(amountInInputBox2 * (rates[currencyInDropDown1] / rates[currencyInDropDown2])));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <h1>Coinverter</h1>
+      <CurrencyInput 
+        listOfCurrencies={Object.keys(rates)}
+        amount={amountInInputBox1}
+        currency={currencyInDropDown1}
+        onAmountChange={handleAmountInInputBox1Change}
+        onCurrencyChange={handleCurrencyInDropDown1Change}
+      />
+      <CurrencyInput 
+        listOfCurrencies={Object.keys(rates)}
+        amount={amountInInputBox2}
+        currency={currencyInDropDown2}
+        onAmountChange={handleAmountInInputBox2Change}
+        onCurrencyChange={handleCurrencyInDropDown2Change}
+      />
     </div>
   );
 }
-
-export default App;
---
+++ src/CurrencyInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import './currencyInput.css';
+
+
+export default function CurrencyInput(props) {
+    return (
+        <div className='input-container'>
+            <input type="text" value={props.amount} onChange={(e) => props.onAmountChange(e.target.value)} />
+            <select value={props.currency} onChange={(e) => props.onCurrencyChange(e.target.value)}>
+                {props.listOfCurrencies.map(currency => (
+                    <option key={currency} value={currency}>{currency}</option>
+                ))}
+            </select>
+        </div>
+    )
+}
+
+// Install 'prop-types': Command - yarn add prop-types
+CurrencyInput.propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+    listOfCurrencies: PropTypes.array,
+    onAmountChange: PropTypes.func,
+    onCurrencyChange: PropTypes.func,
+}
